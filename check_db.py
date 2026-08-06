93f19c739772946575bf498543b7c49c40bbfd44
Bharath Reddy <bharathreddy94911@gmail.com>
2026-08-06 17:09:20 +0530

    Delete check_db.py

--- check_db.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(dbname='metroflow', user='postgres', password='postgres', host='localhost', port='5432')
-cur = conn.cursor()
-cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
-tables = [r[0] for r in cur.fetchall()]
-print("Tables:", tables)
-conn.close()
